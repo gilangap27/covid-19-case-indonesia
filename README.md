7aaec55aacf4d584cc8489c4e4378de8e12fae69
gilangap27 <harzthdev01@gmail.com>
2022-06-01 12:14:18 +0700

    move API to mathdroid API

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-
 # Covid 19 Cases Indonesia
-Website sederhana tentang Covid 19 cases data negara Indonesia dengan mengunakan Covid 19 API, web ini menampilkan Confirmed, Recovered, Death data dan juga menerangkan area, population, location dari negara Indonesia
+
+Website sederhana tentang Covid 19 cases data negara Indonesia dengan mengunakan Covid 19 API, web ini menampilkan Confirmed, Recovered, Death data dan data negara Indonesia
 
 ## Teknologi yang digunakan
+
 - HTML
 - Javascript
 - [Bootstrap]
@@ -10,5 +11,5 @@ Website sederhana tentang Covid 19 cases data negara Indonesia dengan mengunakan
 
 Terima Kasih :grinning: :grinning:
 
-[Bootstrap]: <https://getbootstrap.com/>
-[Covid 19 API]: <https://github.com/M-Media-Group/Covid-19-API>
+[bootstrap]: https://getbootstrap.com/
+[covid 19 api]: https://covid19.mathdro.id/api
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,37 @@
-
 //Dom Selection
-const confirmed = document.querySelector('.confirmed')
-const death = document.querySelector('.death')
-const recovered = document.querySelector('.recovered')
-const area = document.querySelector('.area')
-const population = document.querySelector('.population')
-const loca = document.querySelector('.location')
-const date = document.querySelector('.date')
+const confirmed = document.querySelector(".confirmed");
+const death = document.querySelector(".death");
+const recovered = document.querySelector(".recovered");
+const area = document.querySelector(".area");
+const population = document.querySelector(".population");
+const loca = document.querySelector(".location");
+const date = document.querySelector(".date");
 
 // Get data from API
-fetch('https://covid-api.mmediagroup.fr/v1/cases?country=Indonesia')
-  .then(response => response.json())
-  .then(response => {
-    let data = response.All
+fetch("https://covid19.mathdro.id/api/countries/Indonesia")
+  .then((response) => response.json())
+  .then((response) => {
+    let data = response;
+    console.log(data);
 
     // Data
-    let confirmedData = numberFormat(data['confirmed'])
-    let deathData = numberFormat(data['deaths'])
-    let recoveredData = numberFormat(data['recovered'])
-    let areaData = numberFormat(data['sq_km_area'])
-    let populationData = numberFormat(data['population'])
-    let dateData = data['updated'].slice(0, 11)
+    let confirmedData = numberFormat(data.confirmed.value);
+    let deathData = numberFormat(data.deaths.value);
+    let recoveredData = numberFormat(data.recovered.value);
+    let areaData = numberFormat(7810000);
+    let populationData = numberFormat(275400000);
+    let location = "Southeast Asia";
+    let dateData = data.lastUpdate;
 
     // Set data to HTML
-    confirmed.innerHTML = confirmedData
-    death.innerHTML = deathData
-    recovered.innerHTML = recoveredData
-    area.innerHTML = areaData + ' km²'
-    population.innerHTML = populationData
-    loca.innerHTML = data['location']
-    date.innerHTML = dateData
-  })
+    confirmed.innerHTML = confirmedData;
+    death.innerHTML = deathData;
+    recovered.innerHTML = recoveredData;
+    area.innerHTML = areaData + " km²";
+    population.innerHTML = populationData;
+    loca.innerHTML = location;
+    date.innerHTML = dateData.slice(0, 10);
+  });
 
 function numberFormat(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
